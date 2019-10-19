155891dc806c0137f796705763536166c7ae2c2b
Bryan Gautama <gautamabryan@gmail.com>
2019-10-20 00:31:22 +0700

    :p

---
+++ app/src/main/java/com/example/salur/Profil.java
@@ -0,0 +1,28 @@
+package com.example.salur;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Profil extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_profil,container,false);
+        return view;
+    }
+}
